1727109105a6d4cd3823de6e80f502465f3a5f0f
Nanasi755 <127184916+Nanasi755@users.noreply.github.com>
2024-12-11 18:54:56 +0900

    config.js

--- proximity_vc-1.2.0-bdfacfe4/server_proximity_vc/config.js
+++ proximity_vc-1.2.0-bdfacfe4/server_proximity_vc/config.js
@@ -1,5 +1,5 @@
-const app_id = "ココに記入" //skywayのアプリケーションID //skyway application ID
-const secret_key = "ココに記入" //skywayのシークレットキー // skyway secret key
+const app_id = "6f0e4f0d-c51f-4789-98e8-20efdccd5d4b" //skywayのアプリケーションID //skyway application ID
+const secret_key = "pRFocumR0Ztq9gKcDDM809vWANZpS7mS8+4AGNnYX10=" //skywayのシークレットキー // skyway secret key
 let proximity = true;  //近接VCのtrue:有効/false:無効(デフォルトはtrue) //Proximity VC true:enable/false:disable (default: true)
 let password = false; //接続時のパスワードの有無(デフォルトはfalse) //Password in connection (default: false)
 let distance = 6; // 声の届く最大距離(デフォルトは6) //Max distance of sound (default: 6)
